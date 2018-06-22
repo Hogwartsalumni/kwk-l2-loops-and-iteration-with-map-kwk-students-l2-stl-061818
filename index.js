e43fc2d00974a4141ef2f7a68625220b270b92ee
Scarlett Patton <scarwars05@gmail.com>
2018-06-22 14:02:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ function nameToAttributes(name){
   return name.map(firstAndLast);
 }
 
-function attributsToPhrase(studentArray) {
-  return studentArray.map(function (person)); {
-    return `${person.name} is from ${person.hometown}`;
-  }
-}
+//function attributsToPhrase(studentArray) {
+//  return studentArray.map(function (person)); {
+//    return `${person.name} is from ${person.hometown}`;
+//  }
+//}
